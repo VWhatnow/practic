8721cd8e3a3fdcf10c7c8a89717fa169f73b9015
Баргамин Владислав Константинович <v.bergamin@mkit.local>
2025-03-31 13:33:42 +0300

    GET Endpoint

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ app.use(express.json())
 app.get('/',(req,res)=>{
     res.send(`hello world`)
 })
-
+app.get('/fullname',(req,res)=>{
+    res.send(`Влад Баргамин`)
+})
 app.listen(process.env.PORT || 8080, () => 
 console.log(`Server start no ${process.env.PORT || 8080}`
 ))
