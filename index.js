64c0c8d8423292e709b3a60d0041053f96fc8f17
Баргамин Владислав Константинович <v.bergamin@mkit.local>
2025-03-31 13:33:10 +0300

    mistake

--- index.js
+++ index.js
@@ -5,9 +5,7 @@ app.use(express.json())
 app.get('/',(req,res)=>{
     res.send(`hello world`)
 })
-app.get('/fullname',(req,res)=>{
-    res.send(`Влад Баргамин`)
-})
+
 app.listen(process.env.PORT || 8080, () => 
 console.log(`Server start no ${process.env.PORT || 8080}`
 ))
